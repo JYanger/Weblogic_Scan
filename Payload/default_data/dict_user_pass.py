9a311255f1785f721198468dd1742224ac5d550e
Jyanger <41140615+JYanger@users.noreply.github.com>
2020-11-27 16:30:55 +0800

    Add files via upload

---
+++ Payload/default_data/dict_user_pass.py
@@ -0,0 +1,9 @@
+#coding:utf-8
+#!/usr/bin/env
+
+#https://github.com/JYanger/
+
+username = ['weblogic']
+
+password = ['weblogic','weblogic1','weblogic0','weblogic123','Oracle@123',
+            'weblogic@123','Weblogic123','Weblogic@123']
